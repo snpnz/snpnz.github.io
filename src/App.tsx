4adcf4406caa9fbebbe927d5860f56e48415edfb
Nikolay Fedoseev EnvyBook <xneek@mail.ru>
2021-10-25 22:20:33 +0300

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import {
+  getAuthLink,
+  getAuthTokenAsync,
+  setAuthTokenByCodeAsync,
+  getUser
+} from './services/authService';
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [dbLoading, setDbLoading] = useState(true);
+  const [err, setErr] = useState('');
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
+
+    if(code?.length) {
+      const getUserData = async () => {
+        setLoading(true);
+        try {
+          await setAuthTokenByCodeAsync(code);
+          window.history.pushState({},'', '/');
+          window.location.reload();
+          //window.location.href = window.location.href.replace(window.location.search, '');
+        } catch (e) {
+          setErr((e as Error).message)
+          window.location.href = getAuthLink();
+        }
+        finally {
+          setLoading(false);
+        }
+      }
+      getUserData();
+    } else {
+      const getStoredData = async () => {
+        setLoading(true);
+        try {
+          const token = await getAuthTokenAsync();
+          setToken(token);
+        } catch (e) {
+          console.log(e)
+          //window.location.href = getAuthLink();
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      getStoredData();
+    }
+
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {err && <p>{err}</p>}
+        {token && <pre>{JSON.stringify(getUser(),null, '\t')}</pre>}
+        {!token && <button  className="bigButton" onClick={() => window.location.href = getAuthLink()}>Войти в Strava</button>}
+      </div>
   );
 }
 
---
+++ src/services/authService.ts
@@ -0,0 +1,91 @@
+const client_id = '73436';
+const client_secret = 'ba9fb913d81fc6941fe0d6e96011de332fff2697';
+const lsTokenKey = 'snpnz_token';
+const lsUserKey = 'snpnz_user';
+
+export function getAuthLink() {
+    const data = {
+        client_id,
+        redirect_uri: window.location.origin,
+        response_type: 'code',
+        approval_prompt : 'auto',
+        scope: 'activity:read',
+    };
+
+    const searchParams = new URLSearchParams();
+    Object.entries(data).forEach(([key, val]) => searchParams.append(key, val))
+
+    return `https://www.strava.com/oauth/authorize?${searchParams.toString()}`;
+}
+
+export async function getAuthTokenAsync() {
+    const lsTokenDataStr = localStorage.getItem(lsTokenKey);
+
+    if (lsTokenDataStr && lsTokenDataStr.length) {
+        const tData = JSON.parse(lsTokenDataStr);
+        if (tData.expires_at && tData.expires_at * 1000 > Date.now()) {
+            return tData.access_token;
+        }
+
+        if (tData.refresh_token) {
+
+                const result = await fetch("https://www.strava.com/api/v3/oauth/token", {
+                    body: new URLSearchParams({
+                        client_id,
+                        client_secret,
+                        refresh_token: tData.refresh_token,
+                        grant_type: 'refresh_token'
+                    }).toString(),
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    method: "POST"
+                }).then(res => res.json());
+                localStorage.setItem(lsTokenKey, JSON.stringify(result));
+
+                return result.access_token;
+        }
+
+        throw new Error('no token');
+    } else {
+        throw new Error('no stored token data');
+    }
+}
+
+export async function setAuthTokenByCodeAsync(code:string) {
+        const result = await fetch("https://www.strava.com/api/v3/oauth/token", {
+            body: new URLSearchParams({
+                client_id,
+                client_secret,
+                code,
+                grant_type: 'authorization_code'
+            }).toString(),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            method: "POST"
+        }).then(res => res.json());
+
+        console.log({result})
+        const { athlete, ...tokenData } = result;
+
+        localStorage.setItem(lsUserKey, JSON.stringify(athlete));
+        localStorage.setItem(lsTokenKey, JSON.stringify(tokenData));
+
+        return result.access_token;
+}
+
+export async function unAuthorizeAsync(){
+    const access_token = '';
+        await fetch(
+            `https://www.strava.com/oauth/deauthorize?${new URLSearchParams({ access_token }).toString()}`,
+            { method: "POST" }
+        ).then(res => res.json());
+        localStorage.clear();
+
+        return Promise.resolve({ success: true });
+}
+
+export function getUser() {
+    return JSON.parse(localStorage.getItem(lsUserKey) as string);
+}
