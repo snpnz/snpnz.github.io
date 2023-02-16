bb66a36d37d6543e8a97da74003174b1116dbd3c
Николай Федосеев <xneek@mail.ru>
2023-02-17 00:26:27 +0300

    dev mode on about

--- src/components/AppAbout/index.tsx
+++ src/components/AppAbout/index.tsx
@@ -1,8 +1,12 @@
 import React, {HTMLAttributes} from 'react';
 import {Typography, Box, Link} from '@mui/material';
+import Button from "@mui/material/Button";
+import {useAppSelector} from "../../store";
 
 const AppAbout: React.FC<HTMLAttributes<HTMLDivElement>> = () => {
-    
+    const [dev, setDev] = React.useState(0);
+    const store = useAppSelector(s => s.main);
+
     return <section>
         <Typography variant="h6" component="h4" gutterBottom sx={{p: 1, mt: 2}}>
             О приложении
@@ -17,6 +21,13 @@ const AppAbout: React.FC<HTMLAttributes<HTMLDivElement>> = () => {
         {/* <Typography variant="body2" component="p" sx={{p: 1}}>
             Связаться с автором в <Link href="https://t.me/xneek" target="_blank">telegram</Link>
         </Typography> */}
+
+            {dev < 10 && <Button color="inherit" onClick={() => setDev(c => c+1)} size="small">
+                Служебная информация
+                {dev > 5 && <strong>{10 - dev}</strong>}
+            </Button>}
+
+            {dev > 10 && <pre>{JSON.stringify(store, null ,'  ')}</pre>}
         </Box>
     </section>
 }
