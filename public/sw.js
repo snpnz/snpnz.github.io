594120d27aa50728cbb0ac42c2a2c73edccfcaef
Николай Федосеев <xneek@mail.ru>
2023-02-21 00:12:12 +0300

    offline fix

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 'use strict';
-const CACHE_STATIC = 'static-cache-v2';
-const files = [
+const cacheName = 'static-cache-v4';
+const precacheResources = [
     './',
     "android-chrome-192x192.png",
     "android-chrome-512x512.png",
@@ -20,58 +20,24 @@ const files = [
     './index.html',
     './sw.js',
 ];
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!');
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activate event!');
+});
 
-function hndlEventInstall(evt) {
-    async function cacheStaticFiles() {
-        const cacheStat = await caches.open(CACHE_STATIC);
-        await Promise.all(
-            files.map(function (url) {
-                return cacheStat.add(url).catch(function (reason) {
-                    console.log(`'${url}' failed: ${String(reason)}`);
-                });
-            })
-        );
-    }
-
-    //  wait until all static files will be cached
-    evt.waitUntil(cacheStaticFiles());
-}
-
-function hndlEventFetch(evt) {
-    async function getFromCache() {
-        const cache = await self.caches.open(CACHE_STATIC);
-        const cachedResponse = await cache.match(evt.request);
-        if (cachedResponse) {
-            return cachedResponse;
-        }
-        // wait until resource will be fetched from server and stored in cache
-        const resp = await fetch(evt.request);
-        // await cache.put(evt.request, resp.clone());
-        return resp;
-    }
-
-    evt.respondWith(getFromCache());
-}
-
-self.addEventListener('install', hndlEventInstall);
-self.addEventListener('fetch', hndlEventFetch);
-self.addEventListener('push', function(e) {
-    var options = {
-        body: 'This notification was generated from a push!',
-        icon: 'stravastat.png',
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: '2'
-        },
-        actions: [
-            {action: 'explore', title: 'Explore this new world',
-                icon: 'stravastat.png'},
-            {action: 'close', title: 'Close',
-                icon: 'stravastat.png'},
-        ]
-    };
-    e.waitUntil(
-        self.registration.showNotification('Hello world!', options)
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }),
     );
 });
